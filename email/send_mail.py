81282ac92e0eb667aea0f93858a133f2cfb51ae7
geksong <zengzhangsong@gmail.com>
2014-04-14 21:42:36 +0800

    增加发送邮件实例

---
+++ email/send_mail.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+#!/usr/bin/env python
+'''
+File: send_mail.py
+Author: geksong
+Description: 发送邮件
+'''
+def send_mail(mail_address, content):
+	"""
+	向指定地址发送邮件内容
+	"""
+	print "已向%s发送邮件，内容%s" % (mail_address, content)
+if __name__ == '__main__':
+	send_mail("zengzhangsong@gmail.com", "哈哈哈哈，python测试邮件")
